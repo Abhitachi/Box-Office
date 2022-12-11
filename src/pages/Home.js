1d62173393d7118f77180db2abb7ebd5b416ced6
abhitachi <abhisekshetter1999@gmail.com>
2022-12-11 17:15:14 +0530

    displayed show results and small fetch refractor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
-  return <MainPageLayout>This is Home-Page</MainPageLayout>;
+  // eslint-disable-next-line no-unused-vars
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+      console.log(result);
+    });
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResults()}
+      This is Home-Page
+    </MainPageLayout>
+  );
 };
 
 export default Home;
