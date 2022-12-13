15deef45519dd25a584e8dafc3f68aa18c873cdb
abhitachi <abhisekshetter1999@gmail.com>
2022-12-13 20:48:38 +0530

    added showpage and loading and error message

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,14 +7,29 @@ const Show = () => {
   //   console.log('params', params);
 
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        setShow(results);
+        setIsLoading(false);
+      })
+      .catch(err => {
+        setError(err.message);
+        setIsLoading(false);
+      });
   }, [id]);
 
   console.log('show', show);
+
+  if (isLoading) {
+    return <div>Data is Loading</div>;
+  }
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
   return <div>This is Show-Page</div>;
 };
 
